2df648665ebd991cd5f6e342e588c7e813712d24
Cheng Zhao <zcbenz@gmail.com>
2014-03-17 20:17:07 +0800

    Print error message.

--- src/keytar_posix.cc
+++ src/keytar_posix.cc
@@ -1,6 +1,7 @@
 #include "keytar.h"
 
 #include <gnome-keyring.h>
+#include <stdio.h>
 
 namespace keytar {
 
@@ -14,19 +15,26 @@ const GnomeKeyringPasswordSchema kGnomeSchema = {
   }
 };
 
+void PrintError(const gchar* where, GnomeKeyringResult result) {
+  if (result != GNOME_KEYRING_RESULT_OK)
+    fprintf(stderr, "%s: %s\n", where, gnome_keyring_result_to_message(result));
+}
+
 }  // namespace
 
 bool AddPassword(const std::string& service,
                  const std::string& account,
                  const std::string& password) {
-  return gnome_keyring_store_password_sync(
+  GnomeKeyringResult result = gnome_keyring_store_password_sync(
       &kGnomeSchema,
       NULL,  // Default keyring.
       (service + "/" + account).c_str(),  // Display name.
       password.c_str(),
       "service", service.c_str(),
       "account", account.c_str(),
-      NULL) == GNOME_KEYRING_RESULT_OK;
+      NULL);
+  PrintError("AddPassword", result);
+  return result == GNOME_KEYRING_RESULT_OK;
 }
 
 bool GetPassword(const std::string& service,
@@ -39,6 +47,7 @@ bool GetPassword(const std::string& service,
       "service", service.c_str(),
       "account", account.c_str(),
       NULL);
+  PrintError("GetPassword", result);
   if (result != GNOME_KEYRING_RESULT_OK)
     return false;
 
@@ -63,6 +72,7 @@ bool FindPassword(const std::string& service, std::string* password) {
       &raw_passwords,
       "service", service.c_str(),
       NULL);
+  PrintError("FindPassword", result);
   if (result != GNOME_KEYRING_RESULT_OK)
     return false;
 
