4da8257ea870d26d598060fc4429e7d5210aa031
Michelle Tilley <binarymuse@github.com>
2017-04-12 11:12:28 -0700

    async/await-ify tests

--- spec/keytar-spec.js
+++ spec/keytar-spec.js
@@ -9,86 +9,49 @@ describe("keytar", function() {
   var account2 = 'buster2'
   var password2 = 'secret2'
 
-  beforeEach(function() {
-    return Promise.all([
-      keytar.deletePassword(service, account),
-      keytar.deletePassword(service, account2)
-    ])
+  beforeEach(async function() {
+    await keytar.deletePassword(service, account),
+    await keytar.deletePassword(service, account2)
   })
 
-  afterEach(function() {
-    return Promise.all([
-      keytar.deletePassword(service, account),
-      keytar.deletePassword(service, account2)
-    ])
+  afterEach(async function() {
+    await keytar.deletePassword(service, account),
+    await keytar.deletePassword(service, account2)
   })
 
   describe("setPassword/getPassword(service, account)", function() {
-    it("sets and yields the password for the service and account", function() {
-      return keytar.setPassword(service, account, password)
-      .then(function() {
-        return keytar.getPassword(service, account)
-      }).then(function(pass) {
-        assert.equal(pass, password)
-        return keytar.setPassword(service, account, password2)
-      }).then(function() {
-        return keytar.getPassword(service, account)
-      })
-      .then(function(pass) {
-        assert.equal(pass, password2)
-      })
+    it("sets and yields the password for the service and account", async function() {
+      await keytar.setPassword(service, account, password)
+      assert.equal(await keytar.getPassword(service, account), password)
+      await keytar.setPassword(service, account, password2)
+      assert.equal(await keytar.getPassword(service, account), password2)
     })
 
-    it("yeilds null when the password was not found", function() {
-      return keytar.getPassword(service, account)
-      .then(function(result) {
-        assert.equal(result, null)
-      })
+    it("yeilds null when the password was not found", async function() {
+      assert.equal(await keytar.getPassword(service, account), null)
     })
   })
 
   describe("deletePassword(service, account)", function() {
-    it("yields true when the password was deleted", function() {
-      return keytar.setPassword(service, account, password)
-      .then(function() {
-        return keytar.deletePassword(service, account)
-      })
-      .then(function(result) {
-        assert.equal(result, true)
-        return keytar.deletePassword(service, account)
-      })
-      .then(function(result) {
-        assert.equal(result, false)
-      })
+    it("yields true when the password was deleted", async function() {
+      await keytar.setPassword(service, account, password)
+      assert.equal(await keytar.deletePassword(service, account), true)
     })
 
-    it("yields false when the password didn't exist", function() {
-      return keytar.deletePassword(service, account)
-      .then(function(result) {
-        assert.equal(result, false)
-        return keytar.setPassword(service, account, password)
-      })
+    it("yields false when the password didn't exist", async function() {
+      assert.equal(await keytar.deletePassword(service, account), false)
     })
   })
 
   describe("findPassword(service)", function() {
-    it("yields a password for the service", function() {
-      return Promise.all([
-        keytar.setPassword(service, account, password),
-        keytar.setPassword(service, account2, password2)
-      ]).then(function() {
-        return keytar.findPassword(service)
-      })
-      .then(function(result) {
-        assert.include([password, password2], result)
-      })
+    it("yields a password for the service", async function() {
+      await keytar.setPassword(service, account, password),
+      await keytar.setPassword(service, account2, password2)
+      assert.include([password, password2], await keytar.findPassword(service))
     })
 
-    it("yields null when no password can be found", function() {
-      return keytar.findPassword(service)
-      .then(function(result) {
-        assert.equal(result, null)
-      })
+    it("yields null when no password can be found", async function() {
+      assert.equal(await keytar.findPassword(service), null)
     })
   })
 })
